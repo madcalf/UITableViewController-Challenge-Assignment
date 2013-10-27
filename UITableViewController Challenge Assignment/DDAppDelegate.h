b3885d82499fb9f8b72e26087eaaf72ef5221aa9
DDD <dixond@optonline.net>
2013-10-27 09:46:07 -0400

    Initial Commit

---
+++ UITableViewController Challenge Assignment/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  UITableViewController Challenge Assignment
+//
+//  Created by DDD on 2013-10-27.
+//  Copyright (c) 2013 DDD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
